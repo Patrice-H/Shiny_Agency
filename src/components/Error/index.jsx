a5a9852256b11b04c840e7d8341c8c1502134b1e
Patrice-H <https://github.com/Patrice-H>
2022-05-17 23:31:02 +0200

    Styling error page

--- src/components/Error/index.jsx
+++ src/components/Error/index.jsx
@@ -1,8 +1,35 @@
+import styled from 'styled-components';
+import colors from '../../utils/style/colors';
+import { ReactComponent as Svg } from '../../assets/404.svg';
+
+const ErrorContainer = styled.div`
+  margin: 0 50px;
+  background-color: ${colors.backgroundLight};
+  padding: 40px 0 100px;
+  @media (max-width: 768px) {
+    margin: 40px 0;
+  }
+`;
+const ErrorText = styled.p`
+  font-size: 32px;
+  font-weight: 700;
+  text-align: center;
+  margin: 60px 20px;
+`;
+const ErrorImg = styled.div`
+  width: 61%;
+  margin: auto;
+`;
+
 const Error = () => {
   return (
-    <>
-      <h1>Oups cette page n'existe pas !</h1>
-    </>
+    <ErrorContainer>
+      <ErrorText>Oups...</ErrorText>
+      <ErrorImg>
+        <Svg />
+      </ErrorImg>
+      <ErrorText>Il semblerait qu’il y ait un problème</ErrorText>
+    </ErrorContainer>
   );
 };
 
