ded485fc93a9eda2fd456c387d7a070916002fee
Patrice-H <https://github.com/Patrice-H>
2022-05-17 23:21:07 +0200

    Adding a logo picture and styling header component

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,13 +1,42 @@
 import { Link } from 'react-router-dom';
-import './Header.css';
+import styled from 'styled-components';
+import colors from '../../utils/style/colors';
+import logoLight from '../../assets/light-logo.png';
+
+const StyledHeader = styled.header`
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+  justify-content: space-between;
+  align-items: center;
+  margin: 40px 50px;
+  @media (max-width: 768px) {
+    flex-direction: column;
+    margin: 20px auto;
+  }
+`;
+const StyledLink = styled(Link)`
+  padding: 15px;
+  color: #8186a0;
+  text-decoration: none;
+  font-size: 18px;
+  ${(props) =>
+    props.$isFullLink &&
+    `color: white; border-radius: 30px; background-color: ${colors.primary};`}
+`;
 
 const Header = () => {
   return (
-    <nav className="header-navbar">
-      <Link to="/">Accueil</Link>
-      <Link to="/freelances">Profils</Link>
-      <Link to="/survey/1">Faire le test</Link>
-    </nav>
+    <StyledHeader>
+      <img src={logoLight} alt="logo" />
+      <nav className="header-navbar">
+        <StyledLink to="/">Accueil</StyledLink>
+        <StyledLink to="/freelances">Profils</StyledLink>
+        <StyledLink to="/survey/1" $isFullLink>
+          Faire le test
+        </StyledLink>
+      </nav>
+    </StyledHeader>
   );
 };
 
