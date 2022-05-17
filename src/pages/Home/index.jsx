762a636a9637a282f0e12b60bd8c455803b434f2
Patrice-H <https://github.com/Patrice-H>
2022-05-17 23:42:40 +0200

    Adding home page

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,27 +1,95 @@
+import { Link } from 'react-router-dom';
+import styled from 'styled-components';
+import colors from '../../utils/style/colors';
+import { ReactComponent as Svg } from '../../assets/home-illustration.svg';
 import Header from '../../components/Header';
-import logo from '../../logo.svg';
 import './App.css';
+
+const HomeContainer = styled.div`
+  margin: 0 50px;
+  background-color: ${colors.backgroundLight};
+  position: absolute;
+  display: flex;
+  padding-bottom: 140px;
+  justify-content: space-between;
+  @media (max-width: 992px) {
+    padding-bottom: 100px;
+  }
+  @media (max-width: 768px) {
+    flex-direction: column;
+    margin: 30px 0;
+  }
+`;
+const LeftPart = styled.div`
+  width: 48%;
+  @media (max-width: 768px) {
+    width: 80%;
+    margin: auto;
+    order: 2;
+  }
+`;
+const HomeTitle = styled.h1`
+  font-size: 50px;
+  margin: 180px 0 100px 120px;
+  @media (max-width: 992px) {
+    font-size: 40px;
+    margin: 100px 0 70px 50px;
+  }
+  @media (max-width: 768px) {
+    margin: 70px auto 0;
+    text-align: center;
+  }
+`;
+const HomeImg = styled.div`
+  width: 40%;
+  margin: 140px 75px 0 0;
+  @media (max-width: 992px) {
+    margin: 100px 50px 0 0;
+  }
+  @media (max-width: 768px) {
+    width: 70%;
+    margin: 70px auto 0;
+    order: 1;
+  }
+`;
+const StyledLink = styled(Link)`
+  padding: 15px 70px;
+  margin-left: 100px;
+  color: #8186a0;
+  text-decoration: none;
+  font-size: 18px;
+  ${(props) =>
+    props.$isFullLink &&
+    `color: white; border-radius: 30px; background-color: ${colors.primary};`}
+  @media (max-width: 992px) {
+    margin-left: 50px;
+  }
+  @media (max-width: 768px) {
+    display: block;
+    width: fit-content;
+    text-align: center;
+    margin: 50px auto;
+  }
+`;
 
 function Home() {
   return (
     <>
       <Header />
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <HomeContainer>
+        <LeftPart>
+          <HomeTitle>
+            Repérez vos besoins, on s’occupe du reste, avec les meilleurs
+            talents
+          </HomeTitle>
+          <StyledLink to="/survey/1" $isFullLink>
+            Faire le test
+          </StyledLink>
+        </LeftPart>
+        <HomeImg>
+          <Svg />
+        </HomeImg>
+      </HomeContainer>
     </>
   );
 }
