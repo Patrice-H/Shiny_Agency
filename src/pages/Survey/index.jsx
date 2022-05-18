3620e2ff3f2f32efb0dd7c7cd20b996b1ee5d2a4
Patrice-H <https://github.com/Patrice-H>
2022-05-18 14:26:27 +0200

    Update survey page to load data from the api

--- src/pages/Survey/index.jsx
+++ src/pages/Survey/index.jsx
@@ -1,22 +1,59 @@
 import { Link, useParams } from 'react-router-dom';
+import { useState, useEffect } from 'react';
+import styled from 'styled-components';
+import colors from '../../utils/style/colors';
 import Header from '../../components/Header';
+import { Loader } from '../../utils/Atoms';
 import './Survey.css';
 
+const QuestionTitle = styled.h2`
+  text-decoration: underline;
+  text-decoration-color: ${colors.primary};
+  width: fit-content;
+  margin: auto;
+  @media (max-width: 768px) {
+    margin: 40px auto 0;
+  }
+`;
+const QuestionContent = styled.div`
+  width: fit-content;
+  margin: 30px auto;
+  padding: 0 30px;
+`;
+
 const Survey = () => {
-  const { questionNumber } = useParams();
-  const previousLink = `/survey/${parseInt(questionNumber) - 1}`;
-  const nextLink = `/survey/${parseInt(questionNumber) + 1}`;
+  const { questionPage } = useParams();
+  const questionNumber = parseInt(questionPage);
+  const previousLink = `/survey/${questionNumber - 1}`;
+  const nextLink = `/survey/${questionNumber + 1}`;
+  const [questions, setQuestions] = useState({});
+  const [isDataLoading, setDataLoading] = useState(false);
+
+  useEffect(() => {
+    setDataLoading(true);
+    fetch(`http://localhost:8000/survey`).then((response) =>
+      response
+        .json()
+        .then(({ surveyData }) => {
+          setQuestions(surveyData);
+          setDataLoading(false);
+        })
+        .catch((error) => console.log(error))
+    );
+  }, []);
 
   return (
     <>
       <Header />
-      <h1>Questionnaire</h1>
-      <h2>Question n°{questionNumber}</h2>
+      <QuestionTitle>Question n°{questionPage}</QuestionTitle>
+      {isDataLoading ? (
+        <Loader />
+      ) : (
+        <QuestionContent>{questions[questionNumber]}</QuestionContent>
+      )}
       <nav className="survey-navbar">
-        {parseInt(questionNumber) <= 1 ? null : (
-          <Link to={previousLink}>Précédent</Link>
-        )}
-        {parseInt(questionNumber) >= 10 ? (
+        {questionNumber <= 1 ? null : <Link to={previousLink}>Précédent</Link>}
+        {questionNumber >= 10 ? (
           <Link to="/results">Résultats</Link>
         ) : (
           <Link to={nextLink}>Suivant</Link>
